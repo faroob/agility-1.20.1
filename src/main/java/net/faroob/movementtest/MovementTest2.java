b5a1aa52237f8bb9626956182020c618c397cb92
Forest <fkleinrock@gmail.com>
2024-11-19 13:40:20 -0500

    Initial commit

---
+++ src/main/java/net/faroob/movementtest/MovementTest2.java
@@ -0,0 +1,18 @@
+package net.faroob.movementtest;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.faroob.movementtest.networking.ModMessages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MovementTest2 implements ModInitializer {
+	public static final String MOD_ID = "movementtest2";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world");
+		ModMessages.registerC2SPackets();
+	}
+}
---
+++ src/main/java/net/faroob/movementtest/networking/packet/DashC2SPacket.java
@@ -0,0 +1,19 @@
+package net.faroob.movementtest.networking.packet;
+
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+
+import javax.swing.text.html.parser.Entity;
+
+public class DashC2SPacket {
+    public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
+        double pitch = player.getPitch();
+        double yaw = player.getHeadYaw();
+        player.velocityModified = true;
+        player.setVelocity(-Math.sin(Math.toRadians(yaw)) * Math.sin(Math.toRadians(pitch + 90)), -Math.sin(Math.toRadians(pitch)), Math.cos(Math.toRadians(yaw)) * Math.sin(Math.toRadians(pitch + 90)));
+    }
+}
